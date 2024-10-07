4e9f95ae0b0eb3f063c4def05a23d96f725389af
Neelarun Mukherjee <neelarun.m25@gmail.com>
2024-10-06 19:28:36 -0500

    cleaning up old codes and uploading final presentation!

--- Waves/lib/tf_silent.py
+++
@@ -1,5 +0,0 @@
-# suppress tensorflow warnings (must be called before importing tensorflow)
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import tensorflow as tf
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
